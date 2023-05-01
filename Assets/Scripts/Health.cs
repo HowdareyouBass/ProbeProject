1d0edf3d7a07cc6586d78fa064df99f5d434eafc
Artem <tf2kub1k@gmail.com>
2023-05-01 11:12:35 +0300

    Comments Deleted

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -11,12 +11,10 @@ public class Health : MonoBehaviour
     void Start()
     {
         entity = gameObject.GetComponent<EntityScript>().GetEntity();
-        //Debug.Assert(entity != null);
         m_OnDeath = entity.GetEvent(EventName.OnDeath);
         m_OnDamaged = entity.GetEvent<float>(EventName.OnDamaged);
         if (m_OnDeath == null) Debug.LogWarning("No on damaged event on this object", gameObject);
         if (m_OnDamaged == null) Debug.LogWarning("No on death event on this object", gameObject);
-        //Debug.Assert(stats != null);
     }
 
     public void TakeDamage(float amount)
