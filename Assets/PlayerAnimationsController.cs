fc4c7eb69594eb0e695e5c90157432dd37739607
Artem <tf2kub1k@gmail.com>
2023-09-26 11:40:26 +0300

    Switchet to URP and lightning done

---
+++ Assets/PlayerAnimationsController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerAnimationsController : MonoBehaviour
+{
+    [SerializeField] private Animator m_Animator;
+    private NavMeshAgent m_Agent;
+    private int m_SpeedHash;
+    
+    private void Start()
+    {
+        m_SpeedHash = Animator.StringToHash("Speed");
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        m_Animator.SetFloat(m_SpeedHash, m_Agent.velocity.magnitude);
+    }
+}
